9ada224c0446f80da83e00c49be2c6fc297215ec
WindForest <munkored@yandex.ru>
2025-12-29 13:31:53 +0700

    added code_lock in sup

--- src/code_locker_sup.erl
+++ src/code_locker_sup.erl
@@ -29,7 +29,10 @@ init([]) ->
     SupFlags = #{strategy => one_for_all,
                  intensity => 0,
                  period => 1},
-    ChildSpecs = [],
+    ChildSpecs = [#{
+        id => code_lock,
+        start => {code_lock, start_link, []},
+        restart => permanent,
+        type => worker
+    }],
     {ok, {SupFlags, ChildSpecs}}.
-
-%% internal functions
